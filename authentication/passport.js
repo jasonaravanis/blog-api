67d5d32199d2cf1fe345e6b25bb012920afc934f
Jason Aravanis <j.aravanis@icloud.com>
2021-11-15 15:41:50 +1100

    Add decrypted password check to passport local strategy

--- authentication/passport.js
+++ authentication/passport.js
@@ -3,24 +3,33 @@ const LocalStrategy = require("passport-local").Strategy;
 const User = require("../models/User");
 const JWTStrategy = require("passport-jwt").Strategy;
 const ExtractJWT = require("passport-jwt").ExtractJwt;
+const bcrypt = require("bcryptjs");
 const debug = require("debug")("app:authentication/passport.js");
 
 module.exports = function (passport) {
   passport.use(
     new LocalStrategy(function (username, password, cb) {
-      User.findOne({ username, password }).exec((err, user) => {
+      User.findOne({ username }).exec((err, user) => {
         if (err) {
           return cb(err);
         } else if (!user) {
           return cb(null, false, { message: "Username not found" });
         } else {
-          return cb(null, user, { message: "You are now logged in" });
+          // TODO: Check this works after implement hashing on sign up
+          bcrypt.compare(user.password, password, (err, result) => {
+            debug(err, result);
+            if (err) {
+              return cb(err);
+            } else if (!result) {
+              return cb(null, false, { message: "Incorrect password" });
+            } else {
+              return cb(null, user, { message: "You are now logged in" });
+            }
+          });
         }
       });
     })
   );
-
-  // TODO: implement bcrypt password hashing above
 
   passport.use(
     new JWTStrategy(
