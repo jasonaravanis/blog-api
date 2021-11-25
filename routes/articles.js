88540edb4e4b42bd4e0b2898ab9a3bd76a7e1980
Jason Aravanis <j.aravanis@icloud.com>
2021-11-25 17:24:12 +1100

    Add 'ensureAdmin' auth check to protected admin routes

--- routes/articles.js
+++ routes/articles.js
@@ -2,24 +2,35 @@ const debug = require("debug")("app:/routes/articles.js");
 const express = require("express");
 const articleController = require("../controllers/article");
 const router = express.Router();
-const { ensureAuthenticated } = require("../authentication/auth");
+const { ensureAuthenticated, ensureAdmin } = require("../authentication/auth");
 
-// GET all articles
+// GET all published articles (public view)
 router.get("/", articleController.get_articles);
 
 // POST an article
-router.post("/", ensureAuthenticated, articleController.post_article);
+router.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  articleController.post_article
+);
 
 // GET a specific article
 router.get("/:articleID", articleController.get_article);
 
 // PUT an article (update an article)
-router.put("/:articleID", ensureAuthenticated, articleController.put_article);
+router.put(
+  "/:articleID",
+  ensureAuthenticated,
+  ensureAdmin,
+  articleController.put_article
+);
 
 // DELETE an article
 router.delete(
   "/:articleID",
   ensureAuthenticated,
+  ensureAdmin,
   articleController.delete_article
 );
 
